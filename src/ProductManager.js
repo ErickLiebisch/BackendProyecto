55f89258f9c019c121248cff464606f59a360f78
Erick Liebisch <erickliebisch@gmail.com>
2023-11-13 15:45:01 -0500

    segunda entrega

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,8 +1,13 @@
+import * as fs from "fs";
+
+
 class ProductManager{
-constructor(){
-    this.products=[]
+constructor(path){
+    this.path=path;
 }
-addProduct(title,description,price,thumbnail,code,stock){
+async addProduct(title,description,price,thumbnail,code,stock){
+const products= await getJsonFromFile(this.path);
+const oldProduct=products.find((pro)=>code===pro.code);
 const product={
     title:title,
     decription:description,
@@ -12,26 +17,27 @@ const product={
     stock:stock,
     id: Date.now(),
 }
-const oldProduct= this.products.find((pro)=>title===pro.title && description=== pro.description && price===pro.price && thumbnail===pro.thumbnail && code===pro.code && stock===pro.stock);
 if(!title || !description || !price || !thumbnail || !code || !stock ) {
     console.log("Please fill all information")
 }else if(!oldProduct){
-    this.products.push(product)
+    products.push(product)
+    await saveJsonInFile(this.path,products);
 }else{
     console.log("product repeated")
 }
 }
 getProducts(){
-    if(!this.products){
+    if(!getJsonFromFile(this.path)){
         console.log("there is no products")
     }else{
-        console.log(this.products)
-        return this.products;
+        console.log(getJsonFromFile(this.path))
+        return getJsonFromFile(this.path);
     }
     
 }
-getProductById(id){
-    const product= this.products.find((pro)=>id===pro.id)
+async getProductById(id){
+    const products= await getJsonFromFile(this.path)
+    const product= products.find((pro)=>id===pro.id)
     if(!product){
         console.log("not found")
     }else{
@@ -39,16 +45,72 @@ getProductById(id){
     }
 
 }
+async updateProduct(id,change,value){
+    const products= await getJsonFromFile(this.path);
+    let product = this.getProductById(id);
+    if(product){
+        if(change==='title'){
+            product.title=value;
+            await saveJsonInFile(this.path,products)
+        }else if(change==='description'){
+            product.description=value;
+            await saveJsonInFile(this.path,products)
+        }else if(change==='price'){
+            product.price=value;
+            await saveJsonInFile(this.path,products)
+        }else if(change==='thumbnail'){
+            product.thumbnail=value;
+            await saveJsonInFile(this.path,products)
+        }else if(change==='code'){
+            product.code=value;
+            await saveJsonInFile(this.path,products)
+        }else if(change==='stock'){
+            product.stock=value;
+            await saveJsonInFile(this.path,products)
+        }else{
+            console.log("Please enter a valid change")
+        }
+    }else{
+        console.log("product to update not found")
+    }
 }
-function test() {
-    const productmanager= new ProductManager();
-    productmanager.getProducts();
-    productmanager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
-    productmanager.getProducts();
-    productmanager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
-    productmanager.getProductById(1);
-    const productos=productmanager.getProducts(); 
-    const producto=productos[0];
-    productmanager.getProductById(producto.id);
+async deleteProduct(id){
+    const products = await getJsonFromFile(this.path);
+    const product=this.getProductById(id);
+    if(!product || !products){
+        console.log("there is no products")
+    }else{
+        const index= products.indexOf(product);
+        products.splice(index,1);
+        await saveJsonInFile(this.path,products);
+        console.log("the product with the id: "+ id + "was deleted")
+    }
 }
-test();
+}
+const getJsonFromFile= async (path)=>{
+    if(!fs.existsSync(path)){
+        return [];
+    }
+    const content= await fs.promises.readFile(path,'utf-8');
+    return JSON.parse(content);
+};
+const saveJsonInFile=(path,data)=>{
+    const content= JSON.stringify(data,null,'\t');
+    return fs.promises.writeFile(path,content,'utf-8')
+}
+    async function test() {
+    const productmanager= new ProductManager('Products.js');
+    await productmanager.getProducts();
+    await productmanager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
+    await productmanager.getProducts();
+    await productmanager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
+    await productmanager.getProductById(1);
+    const productos= await productmanager.getProducts(); 
+    const producto= productos[0];
+    await productmanager.getProductById(producto.id);
+    await productmanager.updateProduct(producto.id,"title","producto")
+    await productmanager.addProduct("producto prueba","Este es un producto prueba 2",199,"Sin imagen","abc1234",15);
+    const productoBorrar=productos[1];
+    await productmanager.deleteProduct(productoBorrar.id);
+ }
+ test();
